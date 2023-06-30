67bfd9838f4407697871b104d22626ccfff5d125
Isaias Munguambe <isaiasruben971@gmail.com>
2023-06-30 12:58:40 +0000

    Ismael

--- heranca/Teste.java
+++ heranca/Teste.java
@@ -1,4 +1,7 @@
 public class Teste {
         public static void main(String[] args) {
-            System.out.println("Ola, Penaldo");    }
+            System.out.println("Ola, Penaldo"); 
+            System.out.println("Ola, Ismael"); 
+             
+          }
 }
